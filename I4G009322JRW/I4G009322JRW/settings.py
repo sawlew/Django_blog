ff507d37101178b33ff4d6a2b1e86fea48bb2a9d
Soliu Adeosun <sawlew@gmail.com>
2022-06-25 21:10:11 +0100

    Secured my secret key

--- I4G009322JRW/I4G009322JRW/settings.py
+++ I4G009322JRW/I4G009322JRW/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e_dl3z-__=yavryrm7msf)(pg(q#3g!+&8)28j%5q9yaehb4fm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
